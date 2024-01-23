c4ae057a215a63c0d7021188ba4e24c66f8a7bbb
narekmaralchyan <narekmaralchyan@gmail.com>
2024-01-23 12:44:10 +0400

    remove logs

--- src/firebase/useGetPractice.tsx
+++ src/firebase/useGetPractice.tsx
@@ -8,13 +8,11 @@ const useGetPractice = () => {
     const {data:authUser,isLoading:userLoading} = useGetAuthUserHook();
     let ref;
     if (authUser?.practiceId) {
-        // ref = query(collection(firestore, "practices").withConverter(practiceConverter), where('practiceId', "==",  authUser?.practiceId ));
         ref = query(collection(firestore, "practices").withConverter(practiceConverter));
     }
     const {data, ...queryData} = useFirestoreQueryData(['practiceId', {'practiceId': authUser?.practiceId}], ref,
         {subscribe:true}, { enabled: !!authUser?.practiceId && !!ref});
 
-    console.log(data?.find(item=>item.docId === authUser?.practiceId))
     return {...queryData, data: data?.length ? data.find(item=>item.docId === authUser?.practiceId) : null, isLoading:userLoading || queryData.isLoading}
 
 }
